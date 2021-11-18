782477f89b6e92de34c26f87b0b98841c6a37254
Alejandro Garrido <34369026+katacliny@users.noreply.github.com>
2021-11-18 14:17:43 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,12 @@
+from flask import Blueprint, Flask, jsonify, request
 
+api = Flask(__name__)
+
+
+@api.route("/", methods=["GET"])
+def base(content: str = None):
+    return "OK"
+
+
+if __name__ == "__main__":
+    api.run(host="0.0.0.0")
